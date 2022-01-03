1b556b727267b8adaaf6eeb3e1ebabda4d7b00b2
Bill Mårtensson <bill@magictechnology.se>
2022-01-03 13:04:33 +0100

    start

---
+++ app/src/main/java/se/magictechnology/piaxintro/MainActivity.kt
@@ -0,0 +1,39 @@
+package se.magictechnology.piaxintro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    var thenumber = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val thetext = findViewById<TextView>(R.id.fancyHeaderText)
+
+        thetext.text = "Hurra! Ändrad text"
+
+        findViewById<Button>(R.id.fancyClickButton).setOnClickListener {
+
+            thenumber = thenumber + 1
+            //thenumber++
+            //thenumber += 1
+
+
+            //var intesiffra = "banan"
+            //var ensiffra = intesiffra.toInt()
+
+            val othertext = findViewById<TextView>(R.id.theCenterText)
+
+            othertext.text = "Klickat knapp " + thenumber.toString()
+        }
+
+    }
+
+
+
+}
